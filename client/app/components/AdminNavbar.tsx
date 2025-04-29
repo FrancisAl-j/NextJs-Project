bd55d649e823dda0b9fab99dd208569acea7ed6a
Francis <kikobilas123@gmail.com>
2025-04-29 20:33:05 +0800

    Updated: Done working with Oauth

--- client/app/components/AdminNavbar.tsx
+++ client/app/components/AdminNavbar.tsx
@@ -1,6 +1,8 @@
+import { auth, signIn, signOut } from "@/auth";
 import Link from "next/link";
 
-const AdminNavbar = () => {
+const AdminNavbar = async () => {
+  const session = await auth();
   return (
     <header className="w-full bg-black">
       <nav className="container flex justify-between m-auto items-center">
@@ -8,7 +10,27 @@ const AdminNavbar = () => {
           <img src="/CPOS.png" alt="CPOS-Logo" width={140} height={70} />
         </Link>
 
-        <button>Sign in</button>
+        {session && session?.user ? (
+          <div>
+            <button
+              onClick={async () => {
+                "use server";
+                await signOut();
+              }}
+            >
+              Sign out
+            </button>
+          </div>
+        ) : (
+          <button
+            onClick={async () => {
+              "use server";
+              await signIn("github");
+            }}
+          >
+            Sign in
+          </button>
+        )}
       </nav>
     </header>
   );
